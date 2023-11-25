e0d511d30bc8c05b6ee74670514df7ec7748ca3d
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-11-25 16:15:04 +0200

    fix

--- src/2.ts
+++ src/2.ts
@@ -16,15 +16,11 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -33,7 +29,11 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, depatrtment: string, salary: number) {
+    super(name, depatrtment, salary + 10000);
+  }
 }
 
+const manager = new Manager("Roman", "developer", 10000);
 
 export {};
